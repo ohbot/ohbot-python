2580027fb43389b1cd824a895f8078a75487d414
ohbot <ohbotrobot@gmail.com>
2017-04-03 15:09:48 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name = 'ohbot',
+    packages = ['ohbot'],
+    version = '1',  
+    description = 'description',
+    author = '',
+    author_email = '',
+    url = 'https://github.com/ohbot/ohbotpython.git',
+    download_url = 'https://github.com/ohbot/ohbotpython/archive/v1.0.tar.gz',
+    keywords = ['ohbot', 'robot'],
+    classifiers = [],
+)
+
