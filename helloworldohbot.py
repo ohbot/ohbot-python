6ab1e4ad7c3ee06da481811b9d5473690da2a166
ohbot <ohbotrobot@gmail.com>
2017-04-17 01:32:40 +0100

    Update helloworldohbot.py

--- helloworldohbot.py
+++ helloworldohbot.py
@@ -1,14 +1,24 @@
-import ohbot
+# import the ohbot module
+
+From ohbot import ohbot
+
+# Reset Ohbot
 
 ohbot.reset()
 
+# Move turn ohbot's head and eyes. 
 ohbot.move(1,2)
 ohbot.move(3,1)
 
+# Wait a few seconds for the motors to move
+
 ohbot.wait(2)
 
+# Move head back to the centre and say "Hello World"
 ohbot.move(1,5,1)
-ohbot.say("Good morning")
+ohbot.say("Hello World")
+
+# Slowly increase the brightness of the eyes.
 
 for x in range(0,10):
 
@@ -18,10 +28,13 @@ for x in range(0,10):
     ohbot.eyeColour(0,0,0)
     ohbot.wait(0.2)
 
+    
+
 ohbot.move(1,5,1)
 ohbot.wait(1)
 
 ohbot.say("Now I am running in python you know",False)
+
 for x in range (0,10):
     ohbot.move(3,x)
     ohbot.eyeColour(x,10-x,x)
@@ -40,6 +53,8 @@ ohbot.reset()
 ohbot.say("and ventriloquism",True,False)
 ohbot.eyeColour(0,0,10)
 ohbot.wait(1)
+
+# close ohbot at the end.
 
 ohbot.close()
 
