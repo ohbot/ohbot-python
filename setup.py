11d35feb1eeb07fa532de6063b6ac2d52e428309
Ohbot <ohbotrobot@gmail.com>
2018-02-28 18:11:21 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,4 +13,7 @@ setup(
     download_url = 'https://github.com/ohbot/ohbot-python/archive/2.3.tar.gz',
     keywords = ['ohbot', 'robot'],
     classifiers = [],
+    install_requires=[
+          'pyserial','lxml',
+      ],
 )
