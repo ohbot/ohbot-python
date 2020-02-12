69f630485564e27f6144b9a29e77b15493b513f6
Ohbot <ohbotrobot@gmail.com>
2020-02-12 02:58:23 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,28 @@
 from distutils.core import setup
 
+import platform
+
+if platform.system() == "Darwin":
+    includes =['pyserial','lxml','playsound','pyobjc','numpy','comtypes']
+
+if platform.system() == "Windows":
+    includes =['pyserial','lxml','playsound','numpy','comtypes']
+
+if platform.system() == "Linux":
+    includes =['pyserial','playsound','gTTS','pydub']
+
 setup(
     name = 'ohbot',
     packages = ['ohbot'],
-    package_data={'': ['MotorDefinitionsv21.omd','Silence1.wav']},
+      package_data={'': ['OhbotSettings.xml','Silence1.wav','ohbotspeech.wav','OhbotSpeech.csv','ohbot.obe','phonemes.txt','Images/movedown.gif','Images/moveright.gif','Images/off.gif','Images/offsmaller.gif','Images/on.gif','Images/onsmaller.gif','Images/picohlogo.gif','Images/picohlogoOn.gif','Images/picohlogoSmall.gif','Images/calibrate400.gif','Images/plus.gif','Images/resetIcon.gif','Images/savebutton.gif','Images/logoPT.gif','MotorDefinitionsv21.omd','Images/pixel.gif','Sounds/fanfare.wav','Sounds/loop.wav','Sounds/ohbot.wav','Sounds/smash.wav','Sounds/spring.wav']},
     include_package_data=True,
-    version = '3.3',  
-    description = 'Python library for controlling Ohbot on a Pi',
+    version = '4.0',
+    description = 'Python library for controlling an Ohbot Robot',
     author = 'ohbot',
     author_email = 'info@ohbot.co.uk',
-    url = 'https://github.com/ohbot/ohbot.git',
-    download_url = 'https://github.com/ohbot/ohbot-python/archive/3.3.tar.gz',
-    keywords = ['ohbot', 'robot'],
+    url = 'https://github.com/ohbot/ohbot-python',
+    download_url = 'https://github.com/ohbot/ohbot-python/archive/4.0.tar.gz',
+    keywords = ['ohbot', 'robot','picoh'],
     classifiers = [],
-    install_requires=[
-          'pyserial','lxml',
-      ],
+    install_requires= includes,
 )
