f388b3b5be01b01f3501640a98cb30b11dee9805
Mattamoz <mw@whoosh.co.uk>
2024-04-11 16:53:33 +0100

    Revert "Merge branch 'master' of https://github.com/ohbot/ohbot-python"
    This reverts commit 3a6ff13c20cab7b68460014b5ae71a69f26c838a, reversing
    changes made to f995a47883b86623d0b783a9f05b5fe28e4aea5d.

--- setup.py
+++ setup.py
@@ -1,12 +1,9 @@
-from setuptools import setup
+from distutils.core import setup
+
 import platform
 
-# Define your includes per platform
-
-print("This is a " + platform.system() + " system")
-
 if platform.system() == "Darwin":
-    includes =['pyserial','lxml','playsound','numpy','comtypes','requests']
+    includes =['pyserial','lxml','playsound','pyobjc','numpy','comtypes','requests']
 
 if platform.system() == "Windows":
     includes =['pyserial','lxml','playsound','numpy','comtypes','requests']
@@ -15,25 +12,17 @@ if platform.system() == "Linux":
     includes =['pyserial','playsound','gTTS','pydub','requests']
 
 setup(
-    name='ohbot',
-    packages=['ohbot'],
-    package_data={
-        '': ['OhbotSettings.xml', 'Silence1.wav', 'ohbotspeech.wav', 'OhbotSpeech.csv', 'ohbot.obe',
-             'phonemes.txt', 'Images/movedown.gif', 'Images/moveright.gif', 'Images/off.gif',
-             'Images/offsmaller.gif', 'Images/on.gif', 'Images/onsmaller.gif', 'Images/picohlogo.gif',
-             'Images/picohlogoOn.gif', 'Images/picohlogoSmall.gif', 'Images/ohbotlogoSmall.gif',
-             'Images/logoPTOH.gif', 'Images/calibrate400.gif', 'Images/plus.gif', 'Images/resetIcon.gif',
-             'Images/savebutton.gif', 'Images/logoPT.gif', 'MotorDefinitionsv21.omd', 'Images/pixel.gif',
-             'Sounds/fanfare.wav', 'Sounds/loop.wav', 'Sounds/ohbot.wav', 'Sounds/smash.wav', 'Sounds/spring.wav']
-    },
+    name = 'ohbot',
+    packages = ['ohbot'],
+      package_data={'': ['OhbotSettings.xml','Silence1.wav','ohbotspeech.wav','OhbotSpeech.csv','ohbot.obe','phonemes.txt','Images/movedown.gif','Images/moveright.gif','Images/off.gif','Images/offsmaller.gif','Images/on.gif','Images/onsmaller.gif','Images/picohlogo.gif','Images/picohlogoOn.gif','Images/picohlogoSmall.gif','Images/ohbotlogoSmall.gif','Images/logoPTOH.gif','Images/calibrate400.gif','Images/plus.gif','Images/resetIcon.gif','Images/savebutton.gif','Images/logoPT.gif','MotorDefinitionsv21.omd','Images/pixel.gif','Sounds/fanfare.wav','Sounds/loop.wav','Sounds/ohbot.wav','Sounds/smash.wav','Sounds/spring.wav']},
     include_package_data=True,
-    version='4.0.12',
-    description='Python library for controlling an Ohbot Robot',
-    author='ohbot',
-    author_email='info@ohbot.co.uk',
-    url='https://github.com/ohbot/ohbot-python',
-    download_url='https://github.com/ohbot/ohbot-python/archive/4.0.12.tar.gz',
-    keywords=['ohbot', 'robot', 'picoh'],
-    classifiers=[],
-    install_requires=includes,
+    version = '4.0.8',
+    description = 'Python library for controlling an Ohbot Robot',
+    author = 'ohbot',
+    author_email = 'info@ohbot.co.uk',
+    url = 'https://github.com/ohbot/ohbot-python',
+    download_url = 'https://github.com/ohbot/ohbot-python/archive/4.0.2.tar.gz',
+    keywords = ['ohbot', 'robot','picoh'],
+    classifiers = [],
+    install_requires= includes,
 )
